b5bb7da801bfabb0eb2d29d2948a0561010d81c1
Erich Schlaepfer <114402221+erichs890@users.noreply.github.com>
2026-01-24 23:57:26 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,8 +4,8 @@ import React from 'react';
 const Footer = () => {
     return (
         <footer>
-            <p>Feito por: <span className="author">Erich Schlaepfer</span></p>
-        </footer>
+            <p>Feito por: <span className="author"><a href="https://github.com/erichs890" target="_blank">Erich Schlaepfer</a></span></p>
+        </footer >
     );
 };
 
